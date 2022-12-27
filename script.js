f3aeba9c78775fec891c8f50716a09aa8c53583d
Markus Heiss <m.heiss@outlook.com>
2022-12-27 17:29:55 +0100

    finished the javascript section

--- script.js
+++ script.js
@@ -1,18 +1,120 @@
 "use strict";
 
-const btn = document.querySelector(".btn-share");
-const form = document.querySelector(".fact-form");
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
 
-btn.addEventListener("click", function (e) {
-    if (form.classList.contains("hidden")) {
-        form.classList.remove("hidden");
-        btn.textContent = "Close";
-    } else {
-        form.classList.add("hidden");
-        btn.textContent = "Share a fact";
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+// Selecting DOM elements
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+const factsList = document.querySelector(".facts-list");
+
+// Create DOM elements: Render facts in list
+factsList.innerHTML = "";
+
+async function loadFacts() {
+  const res = await fetch(
+    "https://jplcldtgweqxgpujwfbs.supabase.co/rest/v1/facts",
+    {
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwbGNsZHRnd2VxeGdwdWp3ZmJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE5NTczNzEsImV4cCI6MTk4NzUzMzM3MX0.fh2_vcj96Z1xE7R-D3BzyOJqFJVuHdCxStFWB_bmM6Y",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwbGNsZHRnd2VxeGdwdWp3ZmJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE5NTczNzEsImV4cCI6MTk4NzUzMzM3MX0.fh2_vcj96Z1xE7R-D3BzyOJqFJVuHdCxStFWB_bmM6Y",
+      },
     }
+  );
+  const data = await res.json();
+  console.log(data);
+  createFactsList(data);
+}
+
+loadFacts();
+
+function createFactsList(facts) {
+  const html = facts
+    .map(
+      (fact) => `
+            <li class="fact">
+              <p>
+                ${fact.text}
+                <a
+                  class="source"
+                  href="${fact.source}"
+                  target="_blank"
+                  >(Source)</a>
+              </p>
+              <span class="tag" style="background-color: ${
+                CATEGORIES.find((cat) => cat.name === fact.category).color
+              }"
+                >${fact.category}</span>
+              <div class="vote-buttons">
+                <button>${fact.votesInteresting} 👍</button><button>${
+        fact.votesMindblowing
+      } 🤯</button
+                ><button>${fact.votesFalse} ⛔️</button>
+              </div>
+            </li>`
+    )
+    .join("");
+
+  factsList.insertAdjacentHTML("afterbegin", html);
+}
+
+// Toggle form visibility
+btn.addEventListener("click", function (e) {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share a fact";
+  }
 });
 
+/*
+ 
 let votesInteresting = 22;
 let votesMindblowing = 23;
 const text = "Lisbon is the capital city of Portugal";
@@ -146,3 +248,5 @@ const CATEGORIES = [
 
 const allCategories = CATEGORIES.map(cat => cat.name);
 console.log(allCategories);
+
+*/
